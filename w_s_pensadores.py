c25fde03b26bf4f1439b8da89c5667cb55a31b1f
ivanzera <oliivgomac@gmail.com>
2021-08-29 23:24:45 -0300

    primeira parte finalizada, falta descobrir o pq de não estar baixando os textos

--- w_s_pensadores.py
+++ w_s_pensadores.py
@@ -10,9 +10,17 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-source = requests.get('https://www.pensador.com/autor/{}')
+source = requests.get('https://www.pensador.com/autor/clarice_lispector/').text
 soup = BeautifulSoup(source, 'html.parser')
 csv_cits = open('cits_pens.csv', 'w') #citações baixadas
 
 csv_writer = csv.writer(csv_cits) #criação do arquivo com as citações
 csv_writer.writerow(['pensador', 'citações','link da citação']) #adc os nomes das colunas
+
+for pensamento in soup.find_all('thought-card'):
+    frase = pensamento.p.text
+
+    print()
+
+    csv_cits.close()
+
