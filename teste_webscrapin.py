a0bf3a91c2b861717397f1ca38b9fd5583d247f1
ivanzera <oliivgomac@gmail.com>
2021-08-29 21:00:14 -0300

    primeira parte do código pra fazer a busca e arquivo modelo pra colocar os primeiros comandos

---
+++ teste_webscrapin.py
@@ -0,0 +1,60 @@
+"""
+SCRIPT DE WEBSCRAPING SEGUINDO O TUTORIAL DE COREY SCHAFER (coreyms.com)
+NO TUTORIAL ELE USA O ARGUMENTO 'lxml' NO LUGAR DE 'html.parser'...
+"""
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+source = requests.get('https://coreyms.com').text #objeto que avisa qual página vc vai pegar as info
+
+soup = BeautifulSoup(source, 'html.parser') #cria o objeto com as informações do html (código fonte)
+
+csv_file = open('cms_scrape.csv', 'w') #esse w é pra 'write csv'
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline', 'summary', 'video_link'])
+
+for article in soup.find_all('article'):
+    headline = article.h2.a.text  # quando adiciona .text o cara tem os dados em formato de plain text
+    print(headline)  # mostra a seção do html que tem a info    desse objeto
+
+    summary = article.find('div',class_='entry-content').p.text  # aqui vc cria um objeto pra ver o resumo (em texto) do artigo no site do cara
+    print(summary)
+
+    '''ESSA FUNÇÃO TRY SERVE PRA """DRIBLAR""" ALGUM LINK QUE NÃO TENHA UM DOS ARGUMENTOS
+    NESSE CASO, ALGUNS ARQUIVOS N VÃO TER LINK DE VÍDEO
+    AÍ ELE SUBSTITUI POR NONE QUANDO FOR PRINTAR A PARADA
+    '''
+
+    # NA VDD A GNT NÃO VAI PRECISAR DESSA PARTE, POR ENQUANTO
+    try:
+        vid_src = article.find('iframe', class_='youtube-player')['src']
+        vid_id = vid_src.split('/')[4]
+        vid_id = vid_id.split('?')[0]
+
+        yt_link = f'https://youtube.com/watch?v={vid_id}'
+    except Exception as e:
+                yt_link = None
+
+    print(yt_link )
+
+    print()
+
+    csv_writer.writerow([headline, summary, yt_link])
+
+csv_file.close()
+
+# comando pra pegar informação de link do vídeo que tem no artigo
+
+# transforma o link do vídeo numa lista. cada elemento do link vira um elemento da lista
+# pega uma determinada informação do link
+
+      # isso é o link real pro vídeo
+
+
+#try funciona como um tipo de exceção
+
+
+#print(article.prettify()) #.prettify mostra as info do html com a mesma formatação
+
---
+++ w_s_pensadores.py
@@ -0,0 +1,18 @@
+'''
+SE ESSE LANCE DER CERTO, VOCÊ VAI PODER BUSCAR E BAIXAR TODAS AS CITAÇÕES DE UM DETERMINADO PERSONAGEM
+SE VAI CONSEGUIR JUNTAR TUDO NUM CSV
+'''
+'''
+A IDEIA É USAR A FUNÇÃO INPUT PRA MUDAR A URL NA SOURCE, O INPUT VAI SER O NOME DA PESSOA QUE VOCÊ QUISER
+'''
+
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+source = requests.get('https://www.pensador.com/autor/{}')
+soup = BeautifulSoup(source, 'html.parser')
+csv_cits = open('cits_pens.csv', 'w') #citações baixadas
+
+csv_writer = csv.writer(csv_cits) #criação do arquivo com as citações
+csv_writer.writerow(['pensador', 'citações','link da citação']) #adc os nomes das colunas
